af1997de87c3accda0ef8ff371a06bddb6031b3b
Nafiul Aziz <nafiulaziz.na@gmail.com>
2025-08-28 13:08:29 +0600

    Sort

---
+++ Sort.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+    vector<int> num = {7,1,5,3,6,4};
+    int n = num.size();
+    
+    for(int i=0; i<n; i++)
+    {
+        for(int j=0; j<n-1; j++)
+        {
+            if(num[j] > num[j+1])
+            {
+                int m = num[j];
+                num[j] = num[j+1];
+                num[j+1] = m;
+            }
+        }
+    }
+    
+    cout << "Sorted vector: " << endl;
+    
+    for(int x : num)
+     cout << x << endl;
+    
+    return 0;
+}
